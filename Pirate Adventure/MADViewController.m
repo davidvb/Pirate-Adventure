68c59c69c622a215e94e0ab0090802dc484e5032
Dean Murphy <deano.uk@gmail.com>
2014-03-25 22:33:10 +0000

    Tiles generate with logic to move around the scene and buttons dynamically hide.

--- Pirate Adventure/MADViewController.m
+++ Pirate Adventure/MADViewController.m
@@ -8,6 +8,7 @@
 
 #import "MADViewController.h"
 #import "MADFactory.h"
+#import "MADTile.h"
 
 @interface MADViewController ()
 
@@ -19,9 +20,10 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    MADFactory *array = [[MADFactory alloc]init];
-    [array tiles];
-    
+    MADFactory *factory = [[MADFactory alloc]init];
+    self.tiles = [factory tiles];
+    self.currentPoint = CGPointMake(0, 0);
+    [self updateTile];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,15 +35,56 @@
 }
 
 - (IBAction)northButtonPressed:(UIButton *)sender {
+    
+    self.currentPoint = CGPointMake(self.currentPoint.x, self.currentPoint.y + 1);
+    [self updateTile];
+    
 }
 
 - (IBAction)eastButtonPressed:(UIButton *)sender {
+    
+    self.currentPoint = CGPointMake(self.currentPoint.x + 1, self.currentPoint.y);
+    [self updateTile];
+    
 }
 
 - (IBAction)southButtonPressed:(UIButton *)sender {
+    
+    self.currentPoint = CGPointMake(self.currentPoint.x, self.currentPoint.y - 1);
+    [self updateTile];
+    
 }
 
 - (IBAction)westButtonPressed:(UIButton *)sender {
+    
+    self.currentPoint = CGPointMake(self.currentPoint.x - 1, self.currentPoint.y);
+    [self updateTile];
+    
+}
+
+- (void)updateTile {
+    
+    MADTile *tileModel = [[self.tiles objectAtIndex:self.currentPoint.x] objectAtIndex:self.currentPoint.y];
+    self.storyLabel.text = tileModel.story;
+    [self updateButtons];
+    
+}
+
+- (void)updateButtons {
+    
+    self.northButton.hidden = [self tileExistsAtPoint:CGPointMake(self.currentPoint.x, self.currentPoint.y + 1)];
+    self.eastButton.hidden = [self tileExistsAtPoint:CGPointMake(self.currentPoint.x + 1, self.currentPoint.y)];
+    self.southButton.hidden = [self tileExistsAtPoint:CGPointMake(self.currentPoint.x, self.currentPoint.y - 1)];
+    self.westButton.hidden = [self tileExistsAtPoint:CGPointMake(self.currentPoint.x - 1, self.currentPoint.y)];
+}
+
+- (BOOL)tileExistsAtPoint:(CGPoint)point {
+    
+    if (point.y >= 0 && point.x >=0 && point.x < [self.tiles count] && point.y < [[self.tiles objectAtIndex:point.x] count]) {
+        return NO;
+    } else {
+        return YES;
+    }
 }
 
 @end
